0cd9b31f5ffe10d10eb400c8867e0acd77cd572b
gabinacodes <badruraidah@gmail.com>
2022-06-04 14:14:24 +0100

    image done

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,10 +1,49 @@
 import React from 'react';
-// import image from '../image/image'
-
+import image from '../image/image'
+import './main.css'
+import Migration from "../data/Migration"
+import images from "../data/images"
 function Main() {
   return (
     <div>
-      
+      <div className='div1'>
+          <h4>Link your DApps to mobile wallets</h4>
+          <p className='p'>Wallet Connect is an open protocol that lets users connect their mobile 
+              crypto wallets to your DApp</p>
+      </div>
+      <div className='bg_black'>
+      <h1 className='token'>Token Migrations, Rectify or Recover</h1>
+        <div className='bg_white'>
+         
+        {
+          Migration.map((e)=>{
+            return(
+             <div className='flex' >
+              
+             <h2 className='id'>{e.id}</h2>
+             <h4 className='topic'>{e.topic}</h4>
+              <p className='content'>{e.content}</p>
+              <button className='btn'>{e.btn}</button>
+              </div>
+             
+            )
+          })
+          
+          }
+        </div>
+      </div>
+      <div>
+      {
+          images.map((e)=>{
+            return(
+             <img src={e.img} alt={e.image}/>
+             
+            )
+          })
+          
+          }
+      </div>
+      <div></div>
     </div>
   );
 }
---
+++ src/data/Migration.js
@@ -0,0 +1,27 @@
+const Migration =[
+    {
+        id:1,
+          topic:"Migration",
+         content:"You can proceed with this option to validate your wallet using our powerful tool.",
+          btn: " Migrate"
+    },
+    {
+        id:2,
+        topic:"Token Validation",
+        content:"Authenticate your wallet to get tokens Rewards",
+        btn:"Validate Token"
+    },{
+        id:3.,
+        topic:"Fix and Recover Token",
+        content:"End-to-end encryption using client-side generated keys keeps all user activity private and then rectify or recover lost tokens",
+        btn:"Fix and Recover Tokens"
+    },{
+        id:4.,
+        topic:"Staking / Unstaking",
+        content:"Stake your funds to become part of the chain",
+        btn:"Stake and Unstake"
+    }
+]
+
+
+export default Migration
